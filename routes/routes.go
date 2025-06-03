5f271fea1e8af06f33afe2029ed03c39fdab8602
chunwei <chungweeeei@gmail.com>
2025-06-03 17:33:23 +0800

    feat: add cors middleware and modified auth url

--- routes/routes.go
+++ routes/routes.go
@@ -3,10 +3,19 @@ package routes
 import (
 	"example.com/restful-server/controllers"
 	"example.com/restful-server/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(server *gin.Engine) {
+
+	// add cors middleware
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5000"}, // Allow all origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
 
 	apiV1 := server.Group("/v1")
 	apiV1.Use(middleware.Authenticate)
@@ -15,9 +24,13 @@ func RegisterRoutes(server *gin.Engine) {
 		{
 			users.GET("/", controllers.GetUsers)
 		}
+		files := apiV1.Group("/files")
+		{
+			files.POST("/upload", controllers.Upload)
+		}
 	}
 
 	// sign-up / login
-	server.POST("/signup", controllers.Signup)
-	server.POST("/login", controllers.Login)
+	server.POST("/v1/signup", controllers.Signup)
+	server.POST("/v1/login", controllers.Login)
 }
